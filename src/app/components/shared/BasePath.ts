1351f3e59638e388c9a13578e7d5d3627d716eb7
Irfan Ahmed <irfan.pak@gmail.com>
2023-09-02 13:49:31 -0700

    fixing sanity

--- src/app/components/shared/BasePath.ts
+++ src/app/components/shared/BasePath.ts
@@ -1,5 +1,5 @@
 const BAST_PATH_API =
   process.env.NODE_ENV === "development"
     ? "http://localhost:3000"
-    : "https://shopping-mall-self.vercel.app/";
+    : "https://shopping-mall-self.vercel.app";
 export default BAST_PATH_API;
