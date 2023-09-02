25c59f103cbd481d23ea20ef1b8c05f434f22d3f
Irfan Ahmed <irfan.pak@gmail.com>
2023-09-02 12:14:02 -0700

    clerk key

--- src/app/components/shared/BasePath.ts
+++ src/app/components/shared/BasePath.ts
@@ -1,2 +1,5 @@
-const BAST_PATH_API = "http://localhost:3000";
+const BAST_PATH_API =
+  process.env.NODE_ENV === "development"
+    ? "http://localhost:3000"
+    : "https://shopping-mall-self.vercel.app/";
 export default BAST_PATH_API;
